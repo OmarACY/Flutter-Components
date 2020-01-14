b0f515b395379ca1598b89a55102c36a0365d9e9
Omar <omar@Omars-MacBook-Pro.local>
2020-01-13 21:52:45 -0600

    Alerts

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -8,12 +8,51 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () => _showAlert(context),
+          child: Text('Mostrar Alerta'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_left),
         onPressed: () {
           Navigator.pop(context);
         },
       ),
+    );
+  }
+
+  void _showAlert(BuildContext context){
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(20.0)),
+            title: Text('Titulo'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text('Este es el contenido de la caja de la alerta'),
+                FlutterLogo( size: 100.0 )
+              ],
+            ),
+            actions: <Widget>[
+              FlatButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text('Cancelar')
+              ),
+              FlatButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: Text('Ok')
+              )
+            ],
+          );
+        }
     );
   }
 
