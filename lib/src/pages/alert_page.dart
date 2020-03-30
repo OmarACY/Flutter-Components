035b049521deb7034be863a41123e77c66a93c65
Omar Alejandro Cervantes Yepez <omar@aumenta.mx>
2020-03-30 14:04:48 -0600

    77.Widget - Alert

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -8,12 +8,51 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center (
+        child: RaisedButton(
+          child: Text('Mostrar Alerta'),
+          onPressed: () => _showAlert(context),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+        )
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_left),
         onPressed: () {
           Navigator.pop(context);
         },
       ),
+    );
+  }
+
+  void _showAlert(BuildContext context){
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(20.0)),
+            title: Text('Titulo'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text('Este es el contenido de la alerta'),
+                FlutterLogo( size: 100.0,)
+              ],
+            ),
+            actions: <Widget>[
+              FlatButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: Text('Cancelar')),
+              FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('Ok'))
+            ],
+          );
+        }
     );
   }
 
